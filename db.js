11d029b77c71403e33cd7ea86c77156e05d014b0
MaryannVasilyeva <marvas987@gmail.com>
2016-03-23 18:17:32 -0600

    more changes to the about form

--- db.js
+++ db.js
@@ -7,6 +7,11 @@ var School = new Schema({
 	updated_at: Date
 });
 
+var Comment = new Schema({
+	comment: String
+});
+
 mongoose.model('School', School);
+mongoose.model('Comment', Comment);
 mongoose.connect('mongodb://localhost/school_stuff');
 
--- routes/about.js
+++ routes/about.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+var Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +9,13 @@ router.get('/', function(req, res, next) {
 });
  
  //send a comment to the database
+ router.post('/', function(req, res){
+ 	new Comment({
+ 		comment: req.body.comment
+ 	}).save(function( err, comment ){
+ 		res.redirect('/about');
+ 	});
+ });
 
 
 module.exports = router;
